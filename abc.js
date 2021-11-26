81da2031082ad1a6d91605cc49c44301bcca923c
tjshranjan <73181186+tjshranjan@users.noreply.github.com>
2021-11-26 18:16:22 +0530

    wr

---
+++ abc.js
@@ -0,0 +1,29 @@
+var Person1 = new Object()
+Person1.FirstName = "PF1",
+Person1.LastName = "PL1",
+Person1.OfficeAddress = "POA1"
+
+var Person2 = new Object()
+Person2.FirstName = "PF2",
+Person2.LastName = "PL2",
+Person2.OfficeAddress = "POA2"
+
+var Person3 = new Object()
+Person3.FirstName = "PF3",
+Person3.LastName = "PL3",
+Person3.OfficeAddress = "POA3"
+
+console.log("Person1:")
+console.log("FirstName: " + Person1.FirstName)
+console.log("LastName: " + Person1.LastName)
+console.log("OfficeAddress: " + Person1.OfficeAddress)
+
+console.log("Person2:")
+console.log("FirstName: " + Person2.FirstName)
+console.log("LastName: " + Person2.LastName)
+console.log("OfficeAddress: " + Person2.OfficeAddress)
+
+console.log("Person3:")
+console.log("FirstName: " + Person3.FirstName)
+console.log("LastName: " + Person3.LastName)
+console.log("OfficeAddress: " + Person3.OfficeAddress)
